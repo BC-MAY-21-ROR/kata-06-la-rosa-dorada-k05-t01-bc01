a3825e596c975fce87a98959dc6b9225df8a0687
Heriberto Figueroa Michel <50375089+CUBOFIG@users.noreply.github.com>
2021-09-15 16:48:50 -0500

    Creating the array_texts file
    Co-authored-by: adrianMRJ <adrianMRJ@users.noreply.github.com>
    Co-authored-by: Victor Del Rio <TheViccs@users.noreply.github.com>
    Co-authored-by: Juan Ramón Guzmán Aguilar <JuanequeX@users.noreply.github.com>

---
+++ ruby/array_texts.rb
@@ -0,0 +1,7 @@
+module InstancePrices
+  TEXTS = {
+    'Backstage' => "Backstage passes to a TAFKAL80ETC concert",
+    'Sulfuras' => "Sulfuras, Hand of Ragnaros",
+    'Aged' => "Aged Brie"
+  }
+end
--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,3 +1,5 @@
+require_relative 'array_texts'
+include InstancePrices
 # frozen_string_literal: true
 class GildedRose
   def initialize(items)
@@ -7,19 +9,16 @@ class GildedRose
   def update_quality
     @items.each do |item|
       quality_aging(item)
-      item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.sell_in = item.sell_in - 1 if item.name != TEXTS['Sulfuras']
       sell_negative(item)
     end
   end
 
   def sell_negative(item)
     if item.sell_in.negative?
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-        else
-          item.quality = item.quality - item.quality
-        end
+      if item.name != TEXTS['Aged']
+        return (item.quality = item.quality - 1 if item.quality.positive? && (item.name != TEXTS['Sulfuras'])) if item.name != TEXTS['Backstage']
+        return item.quality = item.quality - item.quality
       elsif item.quality < 50
         item.quality = item.quality + 1
       end
@@ -27,16 +26,15 @@ class GildedRose
   end
 
   def quality_aging(item)
-    return quality(item) if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-
+    return quality(item) if (item.name != TEXTS['Aged']) && (item.name != TEXTS['Backstage'])
     if item.quality < 50
       item.quality = item.quality + 1
-      return met_2(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      return met_2(item) if item.name == TEXTS['Backstage']
     end
   end
 
   def quality(item)
-    item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
+    item.quality = item.quality - 1 if item.quality.positive? && (item.name != TEXTS['Sulfuras'])
   end
   
   def met_2(item)
@@ -45,8 +43,6 @@ class GildedRose
   end
 
 end
-
-
 
 class Item
   attr_accessor :name, :sell_in, :quality
